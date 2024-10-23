4ae90286814bc2d64b4cfdf9c05b0860c6c8eb22
shadowlegend <lyheng0@gmail.com>
2024-10-23 23:06:59 +0700

    chore: refactor to simpler set/remove api

--- lib/aasa_assetlinks_server/inmem_store.ex
+++ lib/aasa_assetlinks_server/inmem_store.ex
@@ -70,136 +70,102 @@ defmodule AasaAssetlinksServer.InmemStore do
     end
   end
 
-  def handle_call({:set_aasa_webcredential, app_id}, _from, state) do
+  defp ensure_aasa_app(nil), do: new_aasa()
+
+  defp ensure_aasa_app({_, app}), do: app
+
+  defp update_applink(app, %{"applink" => %{"components" => [_|_] = components}}),
+    do: %{app | "applink" => %{"components" => components}}
+
+  defp update_applink(app, _),
+    do: %{app | "applink" => nil}
+
+  defp update_aasa_webcredential(app, %{"webcredential" => %{}}),
+    do: %{app | "webcredential" => %{}}
+
+  defp update_aasa_webcredential(app, _),
+    do: %{app | "webcredential" => nil}
+
+  defp ensure_assetlinks_app(nil), do: new_assetlinks()
+
+  defp ensure_assetlinks_app({_, app}), do: app
+
+  defp set_namespace(app, app_id),
+    do: %{app | "namespace" => derive_assetlinks_namespace(app_id)}
+
+  defp update_relation(app, %{"relation" => [_|_] = relations}),
+    do: %{app | "relation" => relations}
+
+  defp update_relation(app, _),
+    do: %{app | "relation" => nil}
+
+  defp update_cert_fingerprints(%{"namespace" => "android_app"} = app, %{"sha256_cert_fingerprints" => [_|_] = fingerprints}),
+    do: %{app | "sha256_cert_fingerprints" => fingerprints}
+
+  defp update_cert_fingerprints(app, _),
+    do: %{app | "sha256_cert_fingerprints" => nil}
+
+  def handle_call({:set_aasa_app, {app_id, config}}, _from, state) do
     app = get_app(:aasa, app_id)
 
-    data =
-      if app do
-        app
-        |> elem(1)
-        |> Map.put("webcredential", %{})
-      else
-        %{new_aasa() | "webcredential" => %{}}
-      end
+    config_set =
+      app
+      |> ensure_aasa_app()
+      |> update_applink(config)
+      |> update_aasa_webcredential(config)
 
     :ets.insert(
       :aasa_assetlinks,
-      {construct_aasa_app_ets_key(app_id), data}
+      {construct_aasa_app_ets_key(app_id), config_set}
     )
 
     {:reply, :ok, state}
   end
 
-  def handle_call({:remove_aasa_webcredential, app_id}, _from, state) do
-    app = get_app(:aasa, app_id)
-
-    if app do
-      :ets.insert(
-        :aasa_assetlinks,
-        {construct_aasa_app_ets_key(app_id), app |> elem(1) |> Map.put("webcredential", nil)}
-      )
-    end
+  def handle_call({:remove_aasa_app, app_id}, _from, state) do
+    :ets.delete(:aasa_assetlinks, construct_aasa_app_ets_key(app_id))
 
     {:reply, :ok, state}
   end
 
-  def handle_call({:set_aasa_applink, {app_id, app_link}}, _from, state) do
-    app = get_app(:aasa, app_id)
-
-    data =
-      if app do
-        app
-        |> elem(1)
-        |> Map.put("applink", %{"components" => app_link["components"]})
-      else
-        %{new_aasa() | "applink" => %{"components" => app_link["components"]}}
-      end
-
-    :ets.insert(
-      :aasa_assetlinks,
-      {construct_aasa_app_ets_key(app_id), data}
-    )
-
-    {:reply, :ok, state}
-  end
-
-  def handle_call({:remove_aasa_applink, app_id}, _from, state) do
-    app = get_app(:aasa, app_id)
-
-    if app do
-      :ets.insert(
-        :aasa_assetlinks,
-        {construct_aasa_app_ets_key(app_id), app |> elem(1) |> Map.put("applink", nil)}
-      )
-    end
-
-    {:reply, :ok, state}
-  end
-
-  def handle_call({:set_assetlinks_relations, {app_id, relations}}, _from, state) do
+  def handle_call({:set_assetlinks_app, {app_id, config}}, _from, state) do
     app = get_app(:assetlinks, app_id)
 
-    data =
-      if app do
-        %{elem(app, 1) | "relation" => relations}
-      else
-        %{new_assetlinks() | "relation" => relations, "namespace" => derive_assetlinks_namespace(app_id)}
-      end
+    config_set =
+      app
+      |> ensure_assetlinks_app()
+      |> set_namespace(app_id)
+      |> update_relation(config)
+      |> update_cert_fingerprints(config)
 
     :ets.insert(
       :aasa_assetlinks,
-      {construct_assetlinks_app_ets_key(app_id), data}
+      {construct_assetlinks_app_ets_key(app_id), config_set}
     )
 
     {:reply, :ok, state}
   end
 
-  def handle_call({:set_assetlinks_fingerprints, {app_id, sha256_fingerprints}}, _from, state) do
-    namespace = derive_assetlinks_namespace(app_id)
+  def handle_call({:remove_assetlinks_app, app_id}, _from, state) do
+    :ets.delete(:aasa_assetlinks, construct_assetlinks_app_ets_key(app_id))
 
-    if namespace == "android_app" do
-      app = get_app(:assetlinks, app_id)
-
-      data =
-        if app do
-          %{elem(app, 1) | "sha256_cert_fingerprints" => sha256_fingerprints}
-        else
-          %{new_assetlinks() | "sha256_cert_fingerprints" => sha256_fingerprints, "namespace" => namespace}
-        end
-
-      :ets.insert(
-        :aasa_assetlinks,
-        {construct_assetlinks_app_ets_key(app_id), data}
-      )
-
-      {:reply, :ok, state}
-    else
-      {:reply, {:error, {:invalid_request, "app_id is not type of android_app", {app_id, sha256_fingerprints}}}, state}
-    end
+    {:reply, :ok, state}
   end
 
-  def set_aasa_webcredential(app_id) do
-    GenServer.call(:global.whereis_name(__MODULE__), {:set_aasa_webcredential, app_id})
+  def set_aasa_app(app_id, config) do
+    GenServer.call(:global.whereis_name(__MODULE__), {:set_aasa_app, {app_id, config}})
   end
 
-  def remove_aasa_webcredential(app_id) do
-    GenServer.call(:global.whereis_name(__MODULE__), {:remove_aasa_webcredential, app_id})
+  def remove_aasa_app(app_id) do
+    GenServer.call(:global.whereis_name(__MODULE__), {:remove_aasa_app, app_id})
   end
 
-  def set_aasa_applink(app_id, applink) do
-    GenServer.call(:global.whereis_name(__MODULE__), {:set_aasa_applink, {app_id, applink}})
+  def set_assetlinks_app(app_id, config) do
+    GenServer.call(:global.whereis_name(__MODULE__), {:set_assetlinks_app, {app_id, config}})
   end
 
-  def remove_aasa_applink(app_id) do
-    GenServer.call(:global.whereis_name(__MODULE__), {:remove_aasa_applink, app_id})
-  end
-
-  def set_assetlinks_relations(app_id, relations) do
-    GenServer.call(:global.whereis_name(__MODULE__), {:set_assetlinks_relations, {app_id, relations}})
-  end
-
-  def set_assetlinks_fingerprints(app_id, sha256_fingerprints) do
-    GenServer.call(:global.whereis_name(__MODULE__), {:set_assetlinks_fingerprints, {app_id, sha256_fingerprints}})
+  def remove_assetlinks_app(app_id) do
+    GenServer.call(:global.whereis_name(__MODULE__), {:remove_assetlinks_app, app_id})
   end
 
   def get_aasa_assetlinks() do
