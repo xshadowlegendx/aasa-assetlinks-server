268b75f5fca6cf9a58faba779bc7f0e0d2ecd59c
shadowlegend <lyheng0@gmail.com>
2024-10-25 21:50:48 +0700

    chore: configure libcluster

--- config/runtime.exs
+++ config/runtime.exs
@@ -9,4 +9,22 @@ if config_env() == :prod do
     region: System.get_env("S3_REGION"),
     access_key_id: System.get_env("S3_ACCESS_KEY_ID"),
     secret_access_key: System.get_env("S3_SECRET_ACCESS_KEY")
+
+  if System.get_env("CLUSTERING_STRATEGY") == "gossip" do
+    config :libcluster, topologies: [
+      gossip: [strategy: Elixir.Cluster.Strategy.Gossip]
+    ]
+  end
+
+  if System.get_env("CLUSTERING_STRATEGY") == "k8s" do
+    config :libcluster, topologies: [
+      k8s: [
+        strategy: Elixir.Cluster.Strategy.Kubernetes.DNS,
+        config: [
+          application_name: "aasa-assetlinks",
+          service: System.get_env("CLUSTERING_STRATEGY_K8S_HEADLESS_SERVICE_NAME", "aasa-assetlinks")
+        ]
+      ]
+    ]
+  end
 end
