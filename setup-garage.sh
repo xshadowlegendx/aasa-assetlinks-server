f911dd42ceb7f1d603b6987e482eb45a9ddb3eaa
shadowlegend <lyheng0@gmail.com>
2024-10-31 19:46:37 +0700

    chore: output s3 credentials to .env file also

--- setup-garage.sh
+++ setup-garage.sh
@@ -12,7 +12,10 @@ docker compose exec garage /garage layout apply --version 1
 
 docker compose exec garage /garage bucket create aasa-assetlinks
 
-docker compose exec garage /garage key create aasa-assetlinks-server
+key_create=$(docker compose exec garage /garage key create aasa-assetlinks-server)
+
+echo S3_ACCESS_KEY_ID=$(echo "$key_create" | grep 'Key ID:' | awk '{ print $3 }') >>.env
+echo S3_SECRET_ACCESS_KEY=$(echo "$key_create" | grep 'Secret key:' | awk '{ print $3 }') >>.env
 
 docker compose exec garage /garage bucket allow --read --write --owner aasa-assetlinks --key aasa-assetlinks-server
 
